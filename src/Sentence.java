36e09f5a02654b40a6753e3bc00864955a757e93
Pavlo Korzh <Pavlo102@mail.ru>
2016-03-10 21:58:49 +0200

    Update Sentence.java

--- src/Sentence.java
+++ src/Sentence.java
@@ -3,13 +3,36 @@
  */
 public class Sentence {
 
-    public static StringBuffer replace(StringBuffer stringBuffer,int start, int end){
+      public static StringBuffer replace(StringBuffer stringBuffer, int number){
 
-        stringBuffer.delete(start, end);
+        int start,end;
+        start =0;
 
-        stringBuffer.insert(start,Word.getWord());
+
+        while (stringBuffer.indexOf(" ",start)!=-1){
+            if (stringBuffer.indexOf(" ",start)!=-1){
+                end = stringBuffer.indexOf(" ",start);
+                if(end-start==number){
+                    //    Производим замену
+                    stringBuffer.delete(start, end);
+                    stringBuffer.insert(start,Word.getWord());
+                    start += Word.getWord().length();
+                } else start = end+1 ;
+            }
+        }
+
+        if (stringBuffer.indexOf(" ",start)==-1){
+            end = stringBuffer.length();
+            if(stringBuffer.length()-start == number){
+                //   Производим замену
+                stringBuffer.delete(start, end);
+                stringBuffer.insert(start,Word.getWord());
+            }
+        }
+
 
         return stringBuffer;
     }
+
 
 }
