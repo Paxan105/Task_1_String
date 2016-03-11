eb284002888d78c7f7adc9f01ffbfdf8239162b5
Pavlo Korzh <Pavlo102@mail.ru>
2016-03-11 10:37:29 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,22 @@ public class Main {
     public static void main(String[] args) {
 
          Scanner scanner = new Scanner(System.in);
-          int number;
 
         System.out.print("Введите начальный текст: ");
-        StringBuffer stringBuffer = new StringBuffer(scanner.nextLine());
+        Sentence sentence = new Sentence(scanner.nextLine());
 
         System.out.print("Введите подстроку для замены: ");
-        Word.setWord(scanner.nextLine());
+        String word = scanner.nextLine();
 
         System.out.print("Введите длину слов которые необходимо заменить: ");
-        number=scanner.nextInt();
+        int number=scanner.nextInt();
 
         scanner.close();
 
-        Sentence.replace(stringBuffer,number);
 
-        System.out.println("Результат: " + stringBuffer);
+        sentence.replace(word,number);
 
-
+        System.out.println(sentence);
 
 
 
